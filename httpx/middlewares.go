0e60d26f7156e2e0351e29cd3af96f199072006e
christian <zengineChris@gmail.com>
2021-01-25 21:17:01 +0100

    fix: add more secure headers

--- httpx/middlewares.go
+++ httpx/middlewares.go
@@ -6,6 +6,9 @@ func SecureHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("X-XSS-Protection", "1; mode=block")
 		w.Header().Set("X-Frame-Options", "deny")
+		w.Header().Set("Referrer-Policy", "origin-when-cross-origin")
+		w.Header().Set("Content-Security-Policy", "default-src https:")
+		w.Header().Set("X-Content-Type-Options", "'nosniff' always;")
 		next.ServeHTTP(w, r)
 	})
 }

85b791e37b63c280cc7f11bc11009b8dc4e7ffca
christian <zengineChris@gmail.com>
2021-05-28 17:29:45 +0200

    feat: removed thinks that was out of the scope of this package

--- httpx/controller.go
+++
@@ -1,9 +0,0 @@
-package httpx
-
-type ControllerContract interface {
-	Index(res Response, req Request)
-	Get(res Response, req Request)
-	Store(res Response, req Request)
-	Update(res Response, req Request)
-	Delete(res Response, req Request)
-}
